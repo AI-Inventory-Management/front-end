8297bde326f915d520878272163cda05c14997d7
Maikindustries <a01750145@itesm.mx>
2022-10-12 16:32:51 -0500

    google maps re rendering fixed

--- src/components/MapComponent.js
+++ src/components/MapComponent.js
@@ -1,4 +1,4 @@
-import { GoogleMap, LoadScript, MarkerF } from "@react-google-maps/api";
+import { GoogleMap, MarkerF, useJsApiLoader } from "@react-google-maps/api";
 import { useEffect, useState } from "react";
 
 var containerStyle = {
@@ -11,19 +11,23 @@ const Map = (props) => {
   // props.onSelectStore -> pasar id a Map.js
   // props.isShowingInfo -> bool if true change the
   // center of the map, this has to occcur once (not iplemented yet)
-
+  const [markers, setMarkers] = useState([]); // array que contiene los marcadores con la info de la bd
   const [center, setCenter] = useState({
     lat: 22.42847,
     lng: -99.12766,
   });
+  const { isLoaded } = useJsApiLoader({
+    // id: "google-map-script",
+    googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_KEY,
+    // libraries: ["geometry", "drawing"],
+  });
+
   // center when the info is shown
   // const center = {
   //   lat: 25,
   //   lng: -94,
   //   // Default value
   // };
-
-  const [markers, setMarkers] = useState([]); // array que contiene los marcadores con la info de la bd
 
   // mapeo de colores según el status del marcador
   const colors = {
@@ -48,22 +52,33 @@ const Map = (props) => {
     getMarkerData();
   }, []);
 
+  if (!isLoaded) {
+    return <div>Pou</div>;
+  }
   return (
-    <LoadScript googleMapsApiKey={process.env.REACT_APP_GOOGLE_MAPS_KEY}>
-      <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={5}>
-        {markers.map((marker) => (
-          <MarkerF
-            key={marker.id}
-            clickable={true}
-            onClick={() => {
-              props.onSelectStore(marker.id);
-            }}
-            position={marker.position}
-            icon={{ url: require(`../images/${colors[marker.status]}`) }}
-          />
-        ))}
-      </GoogleMap>
-    </LoadScript>
+    <GoogleMap
+      mapContainerStyle={containerStyle}
+      center={center}
+      zoom={5}
+      options={{
+        zoomControl: false,
+        streetViewControl: false,
+        fullscreenControl: false,
+        mapTypeControl: false,
+      }}
+    >
+      {markers.map((marker) => (
+        <MarkerF
+          key={marker.id}
+          clickable={true}
+          onClick={() => {
+            props.onSelectStore(marker.id);
+          }}
+          position={marker.position}
+          icon={{ url: require(`../images/${colors[marker.status]}`) }}
+        />
+      ))}
+    </GoogleMap>
   );
 };
 
--- src/pages/Filter.js
+++ src/pages/Filter.js
@@ -7,7 +7,7 @@ import StoreProvider, { StoreContext } from "../components/StoreProvider";
 import { useContext } from "react";
 
 function Filter() {
-  const [,setStoresId,, setStoreName] = useContext(StoreContext);
+  const [, setStoresId, , setStoreName] = useContext(StoreContext);
   const Stores = [
     { id: 1, name: "Oxxo" },
     { id: 2, name: "Tiendita" },
@@ -21,86 +21,86 @@ function Filter() {
   }
   */
 
-  const SetStoreId = async (id,name) => {
+  const SetStoreId = async (id, name) => {
     setStoresId(id);
     setStoreName(name);
-  }
+  };
 
   return (
-      <div className="filter-container">
-        <Navbar title="Inventario Tienda" />
-        <div className="filter-card">
-          <p>Llena los filtros para poder ver el inventario de una tienda</p>
-          <div className="filter-add-button">
-            <BiAddToQueue />
-            <span class="buttontext">Añadir Tienda</span>
-          </div>
-          <table className="filter-table">
-            <td className="filter-results-td">
-              <tr className="filter-lable">Identificador:</tr>
-              <tr className="filter-lable">Nombre:</tr>
-              <tr className="filter-lable">Estado:</tr>
-              <tr className="filter-lable">Municipio:</tr>
-              <tr className="filter-lable">Código Postal:</tr>
-            </td>
-            <td>
-              <tr>
-                <input className="filter-input" name="Id" />
-              </tr>
-              <tr>
-                <input className="filter-input" name="name" />
-              </tr>
-              <tr>
-                <input className="filter-input" name="state" />
-              </tr>
-              <tr>
-                <input className="filter-input" name="municipality" />
-              </tr>
-              <tr>
-                <input className="filter-input" name="CP" />
-              </tr>
-              <tr>
-                <button className="filter-button">Buscar</button>
-              </tr>
-            </td>
-            <td className="filter">
-              <table className="filter-results">
-                <td className="filter-results-td">
-                  <th className="filter-th">Identificador</th>
-                  {Stores.map((store, index) => (
-                    <tr className="filter-tr">{store.id}</tr>
-                  ))}
-                </td>
-                <td className="filter-results-td">
-                  <th className="filter-th">Nombre</th>
-                  {Stores.map((store, index) => (
-                    <tr className="filter-tr">
-                      <p className="filter-p">{store.name}</p>
-                    </tr>
-                  ))}
-                  {Stores.length === 0 && (
-                    <p className="no-stores">No se encontraron tiendas</p>
-                  )}
-                </td>
-                <td className="filter-results-td">
-                  <th className="filter-th">Ver tienda</th>
-                  {Stores.map((store, index) => (
-                    <tr className="filter-tr">
-                      <Link to="/tiendas">
-                        <BiChevronRightSquare
-                          className="filter-show"
-                          key={store.id}
-                          onClick = {() => SetStoreId(store.id, store.name)}
-                        />
-                      </Link>
-                    </tr>
-                  ))}
-                </td>
-              </table>
-            </td>
-          </table>
+    <div className="filter-container">
+      <Navbar title="Inventario Tienda" />
+      <div className="filter-card">
+        <p>Llena los filtros para poder ver el inventario de una tienda</p>
+        <div className="filter-add-button">
+          <BiAddToQueue />
+          <span className="buttontext">Añadir Tienda</span>
         </div>
+        <table className="filter-table">
+          <td className="filter-results-td">
+            <tr className="filter-lable">Identificador:</tr>
+            <tr className="filter-lable">Nombre:</tr>
+            <tr className="filter-lable">Estado:</tr>
+            <tr className="filter-lable">Municipio:</tr>
+            <tr className="filter-lable">Código Postal:</tr>
+          </td>
+          <td>
+            <tr>
+              <input className="filter-input" name="Id" />
+            </tr>
+            <tr>
+              <input className="filter-input" name="name" />
+            </tr>
+            <tr>
+              <input className="filter-input" name="state" />
+            </tr>
+            <tr>
+              <input className="filter-input" name="municipality" />
+            </tr>
+            <tr>
+              <input className="filter-input" name="CP" />
+            </tr>
+            <tr>
+              <button className="filter-button">Buscar</button>
+            </tr>
+          </td>
+          <td className="filter">
+            <table className="filter-results">
+              <td className="filter-results-td">
+                <th className="filter-th">Identificador</th>
+                {Stores.map((store, index) => (
+                  <tr className="filter-tr">{store.id}</tr>
+                ))}
+              </td>
+              <td className="filter-results-td">
+                <th className="filter-th">Nombre</th>
+                {Stores.map((store, index) => (
+                  <tr className="filter-tr">
+                    <p className="filter-p">{store.name}</p>
+                  </tr>
+                ))}
+                {Stores.length === 0 && (
+                  <p className="no-stores">No se encontraron tiendas</p>
+                )}
+              </td>
+              <td className="filter-results-td">
+                <th className="filter-th">Ver tienda</th>
+                {Stores.map((store, index) => (
+                  <tr className="filter-tr">
+                    <Link to="/tiendas">
+                      <BiChevronRightSquare
+                        className="filter-show"
+                        key={store.id}
+                        onClick={() => SetStoreId(store.id, store.name)}
+                      />
+                    </Link>
+                  </tr>
+                ))}
+              </td>
+            </table>
+          </td>
+        </table>
       </div>
+    </div>
   );
 }
 
