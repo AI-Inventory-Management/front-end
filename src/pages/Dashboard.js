cc5e298035b5508a5f214824eaaec2ef027036d2
Jacqueline Zavala <a01750480@itesm.mx>
2022-10-24 16:43:17 -0500

    Dashboard Test

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -7,8 +7,11 @@ function Dashboard() {
     <div className="ds-container">
       <Navbar title="Dashboard" />
       <div>
-        <h2>Inicio</h2>
-        <h2>Aquí va el dashboard</h2>
+        <iframe
+          width="960"
+          height="720"
+          src="https://us-east-1.quicksight.aws.amazon.com/sn/embed/share/accounts/560760329438/dashboards/416ddf5a-5049-46fb-af8f-423da678fa9c?directory_alias=riico-dashboard">
+        </iframe>
       </div>
     </div>
   );
