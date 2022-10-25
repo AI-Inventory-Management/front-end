cee53a76b980c2efe02e0ba63340b251d5843898
a01750264 <a01750246@itesm.mx>
2022-10-25 18:29:44 -0500

    dummy dashboard

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -7,11 +7,21 @@ function Dashboard() {
     <div className="ds-container">
       <Navbar title="Dashboard" />
       <div>
-        <iframe
+        <img
+          src={require("../images/dash1.JPG")}
+          alt="dashboard"
+          style={{ width: "100%" }}
+        />
+        <img
+          src={require("../images/dash2.JPG")}
+          alt="dashboard2"
+          style={{ width: "100%" }}
+        />
+        {/* <iframe
           width="960"
           height="720"
           src="https://us-east-1.quicksight.aws.amazon.com/sn/embed/share/accounts/560760329438/dashboards/416ddf5a-5049-46fb-af8f-423da678fa9c?directory_alias=riico-dashboard">
-        </iframe>
+        </iframe> */}
       </div>
     </div>
   );
