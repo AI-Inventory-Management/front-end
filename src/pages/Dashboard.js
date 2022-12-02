794aa2a0f25868253f480bc81d7dc560af8d8327
a01750264 <a01750246@tec.mx>
2022-12-02 00:47:34 +0000

    Adding dashboard

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -7,22 +7,13 @@ function Dashboard() {
   return (
     <div className="ds-container">
       <Navbar title="Dashboard" />
-      <div>
-        <img
-          src={require("../images/dash1.JPG")}
-          alt="dashboard"
-          style={{ width: "100%" }}
-        />
-        <img
-          src={require("../images/dash2.JPG")}
-          alt="dashboard2"
-          style={{ width: "100%" }}
-        />
-        {/* <iframe
-          width="960"
-          height="720"
+      <div style={{height:"100%"}}>
+        <iframe
+          width="100%"
+	  className="ds-dashboard"
+          height="calc(100% - 80px)"
           src="https://us-east-1.quicksight.aws.amazon.com/sn/embed/share/accounts/560760329438/dashboards/416ddf5a-5049-46fb-af8f-423da678fa9c?directory_alias=riico-dashboard">
-        </iframe> */}
+        </iframe>
       </div>
     </div>
   );
